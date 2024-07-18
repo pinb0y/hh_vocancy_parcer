7e1756a052a41e25a6e89add5ac0a9c371b37452
Pinchuk Sergey <Pinboyer@gmail.com>
2024-07-18 16:00:01 +0300

    add typehints and docstrings to class db_manager

--- src/classes/db_manager.py
+++ src/classes/db_manager.py
@@ -1,40 +1,79 @@
 import psycopg2
 
-from src.settings import config
-
 
 class DbManager:
+    """
+    Класс для управления базой данных
+    """
+    def __init__(self, db_name, params):
+        self.__database_name = db_name
+        self.__params = params
+
     def get_companies_and_vacancies_count(self):
         """
         Получает список всех компаний и количество вакансий у каждой компании.
-        :return:
         """
-        ...
+        with psycopg2.connect(dbname=self.__database_name, **self.__params) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("""
+                SELECT companies.name, COUNT(*) AS vacancies_count FROM companies
+                JOIN vacancies USING (company_id)
+                GROUP BY companies.name
+                """)
+                result = cursor.fetchall()
+        conn.close()
+        return result
 
     def get_all_vacancies(self):
         """
         Получает список всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию.
-        :return:
         """
-        ...
+        with psycopg2.connect(dbname=self.__database_name, **self.__params) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("""
+                SELECT companies.name, vacancies.name, salary_from, salary_to, vacancies.URL FROM vacancies
+                JOIN companies USING (company_id)
+                """)
+                result = cursor.fetchall()
+        conn.close()
+        return result
 
     def get_avg_salary(self):
         """
         Получает среднюю зарплату по вакансиям.
-        :return:
         """
-        ...
+        with psycopg2.connect(dbname=self.__database_name, **self.__params) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("""SELECT CAST(AVG(salary_from) as INT) FROM vacancies""")
+                result = cursor.fetchall()
+        conn.close()
+        return result
 
     def get_vacancies_with_higher_salary(self):
         """
         Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям.
-        :return:
         """
-        ...
 
-    def get_vacancies_with_keyword(self):
+        with psycopg2.connect(dbname=self.__database_name, **self.__params) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("""SELECT * FROM vacancies
+                WHERE salary_from > (SELECT AVG(salary_from) FROM vacancies)
+                ORDER BY salary_from DESC
+                """)
+                result = cursor.fetchall()
+        conn.close()
+        return result
+
+    def get_vacancies_with_keyword(self, keyword: str):
         """
         Получает список всех вакансий, в названии которых содержатся переданные в метод слова, например python.
-        :return:
         """
-        ...
+        with psycopg2.connect(dbname=self.__database_name, **self.__params) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(f"""SELECT * FROM vacancies
+                       WHERE name LIKE '%{keyword}%' OR requirements LIKE '%{keyword}%'
+                       ORDER BY salary_from DESC
+                       """)
+                result = cursor.fetchall()
+        conn.close()
+        return result
