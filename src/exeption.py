0e7bd7817fc5127168d529d59a785ff2b32aeccd
Pinchuk Sergey <Pinboyer@gmail.com>
2024-07-18 16:00:19 +0300

    add typehints and docstrings to class exception

--- src/exeption.py
+++ src/exeption.py
@@ -1,8 +1,14 @@
 class ApiException(Exception):
+    """
+    Основной класс для исключений
+    """
     def __init__(self, massage):
         self.massage = massage
         super().__init__(self, massage)
 
 
 class HhApiException(ApiException):
+    """
+    Класс для исключений парсера HH
+    """
     pass
