e2ec6d2a6214c72dcf9ec6ef80f38d4cf37b8e8d
Pinchuk Sergey <Pinboyer@gmail.com>
2024-07-18 15:59:24 +0300

    add typehints and docstrings to class API

--- src/classes/api.py
+++ src/classes/api.py
@@ -22,24 +22,40 @@ class ParserApi(ABC):
 
     @abstractmethod
     def _get_response(self) -> Response:
+        """
+        Абстрактный метод для подключения к api
+        :return:
+        """
         pass
 
     @abstractmethod
     def get_vacancies(self) -> list[dict]:
+        """
+        Абстрактный метод получения вакансий
+        :return:
+        """
         pass
 
     @staticmethod
     @abstractmethod
-    def _check_status(self) -> bool:
+    def _check_status(response) -> bool:
+        """
+        Абстрактный метод проверки подключения
+        :return: Тру или фолз в зависимости от статуса.
+        """
         pass
 
 
 class HhParserApi(ParserApi):
+    """
+    Класс для парсинга вакансий с сайта HeadHunter
+    """
     def __init__(self) -> None:
+        self.headers = {'User-Agent': 'HH-User-Agent'}
         self.__params = {
+            "area": 113,
             "page": 0,
             "per_page": 100,
-            "area": "113",
             "employer_id": ["9498120",
                             "5920492",
                             "64174",
@@ -55,18 +71,31 @@ class HhParserApi(ParserApi):
 
     @property
     def url(self) -> str:
+        """
+        Свойство возвращающее ссылку для подключения
+        :return: адрес API HH
+        """
         return HH_URL
 
     def _get_response(self) -> Response:
-        return requests.get(self.url, params=self.__params)
+        """
+        Подключение к API HH
+        :return: Возвращает подключение
+        """
+        return requests.get(self.url, headers=self.headers, params=self.__params)
 
     def get_vacancies(self) -> None:
+        """
+        Метод записывает в свойства объекта список словарей с вакансиями
+        :return: None
+        """
         response = self._get_response()
         is_allowed = self._check_status(response)
         if not is_allowed:
             raise HhApiException(f"Ошибка запроса данных. Status Code:{Response.status_code}")
         try:
             while self.__params.get("page") != 20:
+                response = self._get_response()
                 vacancies = response.json()["items"]
                 self.__vacancies.extend(vacancies)
                 self.__params["page"] += 1
@@ -75,8 +104,16 @@ class HhParserApi(ParserApi):
 
     @property
     def vacancies(self):
+        """
+        Геттер списка вакансий
+        :return:
+        """
         return self.__vacancies
 
     @staticmethod
     def _check_status(response: Response) -> bool:
+        """
+        Метод проверяющий подключение
+        :return: тру или фолз в зависимости от статуса
+        """
         return response.status_code == 200
